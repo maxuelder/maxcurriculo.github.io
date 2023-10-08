385b364692a001b90f2638fbb03289643702350b
maxuelder <61887113+maxuelder@users.noreply.github.com>
2023-10-07 22:18:30 -0300

    Add files via upload

---
+++ src/components/Body/informacoes/dadosFormacao.js
@@ -0,0 +1,14 @@
+export const formacao = {
+    titulo: 'Formação Acadêmica:',
+    texto1: 'Graduação: Ciências da computação',
+    texto2: 'Instituição: FEMA',
+    texto3: 'Período: 2019 - 2024'
+}
+
+export const experiencia = {
+    titulo: 'Experiências Profissionais:',
+    texto1: 'Leveros',
+    texto2: 'Assistente de TI',
+    texto3: '2021 - o momento'
+
+}
---
+++ src/components/Body/qualificacoes/dadosQualificacoes.js
@@ -0,0 +1,7 @@
+export const titulo = {
+    titulo: 'Qualificações',
+}
+
+export const texto = {
+    texto: ' Profissional orientado aos negócios com experiência em suporte de T.I, nas áreas de infraestrutura e service desk, com bom relacionamento interpessoal, criatividade e proatividade. Com interesse em migrar para a área de desenvolvimento de software, possuindo conhecimento básico (flutter e react native), desejando obter experiência e aprendizado na mesma.'
+}
